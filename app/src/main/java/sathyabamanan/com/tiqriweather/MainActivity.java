e36601d1f7af93268ef08f2ca38a3af96172765b
sathyabaman <sathyabaman@isaithuli.com>
2017-06-03 12:20:40 +0530

    splash screen done

---
+++ app/src/main/java/sathyabamanan/com/tiqriweather/MainActivity.java
@@ -0,0 +1,26 @@
+package sathyabamanan.com.tiqriweather;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 5000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                MainActivity.this.startActivity(new Intent(MainActivity.this, WeatherList.class));
+                MainActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
